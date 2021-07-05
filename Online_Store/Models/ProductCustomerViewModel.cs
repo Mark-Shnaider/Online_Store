5b592c9801a9d82989a9b7a804e2ea32283060e2
Mark-Shnaider <shnajdermarck@yandex.ru>
2021-07-05 16:46:44 +0700

    Add counter to products.

--- Online_Store/Models/ProductCustomerViewModel.cs
+++ Online_Store/Models/ProductCustomerViewModel.cs
@@ -4,20 +4,21 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
 using Online_Store.Models.Base;
+using System.Globalization;
 
 namespace Online_Store.Models
 {
     public class ProductCustomerViewModel : BaseEntityViewModel<Guid>
     {
-        [Display(Name = "Название продукта")]
         public string Name { get; set; }
-        [Display(Name = "Описание")]
         public string Description { get; set; }
-        [Display(Name = "Цена")]
         public decimal Price { get; set; }
-        [Display(Name = "Количество")]
         public int Quantity { get; set; }
-        [Display(Name = "Категория")]
+        public int Amount { get; set; }
+
+        public string Total { get => (Price * Amount).ToString(CultureInfo.InvariantCulture); }
+
+        public Guid CategoryId { get; set; }
         public string CategoryName { get; set; }
 
         public List<string> Categories { get; set; }
