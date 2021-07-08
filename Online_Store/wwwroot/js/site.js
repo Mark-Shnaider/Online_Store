f6800f5f24fb6660d5439baa2e25db9759242c38
Mark-Shnaider <shnaJderMARCK@yandex.ru>
2021-07-08 22:58:04 +0700

    Transfer JS code to site.js

--- Online_Store/wwwroot/js/site.js
+++ Online_Store/wwwroot/js/site.js
@@ -1,4 +1,68 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿AddToCart = function (element) {
 
-// Write your JavaScript code.
+    var amount = document.querySelector(".product-amount").children[1].valueAsNumber;
+    var ProductId = $(element).data("product-id");
+    var CartId = $(element).data("cart-id");
+    $.ajax({
+        type: "POST",
+        url: urls.AddItemToCart,
+        data: {
+            CartId: CartId,
+            ProductId: ProductId,
+            amount: amount
+        },
+        success: function (data) {
+            if (data.data.isValid == false)
+                alert("На складе нет столько товаров")
+        },
+        error: function () {
+            AjaxError();
+        }
+    });
+
+    $.ajax({
+        type: "GET",
+        url: urls.GetCartItems,
+        data: {
+            Id: CartId
+        },
+        success: function (data) {
+            $('#CartContainer').html(data)
+        },
+
+    });
+};
+
+RemoveFromCart = function (element) {
+
+    var ProductId = $(element).data("product-id");
+    var CartId = $(element).data("cart-id");
+    $.ajax({
+        type: "POST",
+        url: urls.RemoveItemFromCart,
+        data: {
+            CartId: CartId,
+            ProductId: ProductId,
+        },
+        success: function (data) {
+            if (data.data.isValid == false)
+                alert("На складе нет столько товаров")
+        },
+        error: function () {
+            AjaxError();
+        }
+    });
+};
+
+
+function changeValue(target, output, amount, price, num) {
+    let val = parseInt(target.value);
+    if (num < 0 && val <= 1) {
+        return;
+    }
+    val += num;
+    target.value = val;
+
+    let totalInt = val * price;
+    output.value = ('' + totalInt).substring(0, 6);
+}
